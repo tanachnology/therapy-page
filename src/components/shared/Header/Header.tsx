eaf1b6cda0b853c00868fe5353f8cb5b93be1131
nahyoomi <nahyomi2000@gmail.com>
2024-07-29 10:53:17 +0100

    feat: Creación del componente header

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,7 +1,14 @@
+import Logo from "./Logo";
+import Navbar from "@/components/shared/Header/Navbar";
+
 export const Header = () => {
+    const navbar1Items = ["Servicios", "TDAH", "TC"];
+    const navbar2Items = ["Blog", "FAQ", "Contacto"];
     return (
-        <header>
-            <p>Header</p>
+        <header className='flex justify-center items-center '>
+           <Navbar items={navbar1Items}/> 
+           <Logo />
+           <Navbar items={navbar2Items}/>
         </header>
     );
 }
