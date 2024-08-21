12e7d69a6b3b1714f7b12f384cc1bc605c84f59f
nahyoomi <nahyomi2000@gmail.com>
2024-08-21 09:56:54 +0100

    feat: update window scroll

--- src/components/shared/Footer/ScrollButton.tsx
+++ src/components/shared/Footer/ScrollButton.tsx
@@ -5,7 +5,7 @@ const ScrollButton = () => {
     const [isVisible, setIsVisible] = useState(false);
 
     const toggleVisibility = () => {
-      if (window.pageYOffset > 300) {
+      if (window.scrollY > 300) {
         setIsVisible(true);
       } else {
         setIsVisible(false);
